fbf76e063c41bd37d9bfc6fb1b26cec98f6a8f14
Blaise Fringel <ffringel9@gmail.com>
2019-06-05 16:02:55 -0500

    Add some basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Keypad from "./components/Keypad";
+import Result from "./components/Result";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+// test components are rendered without crashing
+describe("App's components are loaded and rendered", () => {
+  it("Keypad renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Keypad />, div);
+  });
+
+  it("Result renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Result />, div);
+  });
 });
